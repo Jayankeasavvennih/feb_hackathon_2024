eb80985191a20069706d7ff9c213ec091be371fd
Jayan Keasav Vennih <135574596+Jayankeasavvennih@users.noreply.github.com>
2024-02-15 11:20:38 +0530

    Update welcome.component.ts

--- youtube_video_summarize/src/app/welcome/welcome.component.ts
+++ youtube_video_summarize/src/app/welcome/welcome.component.ts
@@ -24,8 +24,11 @@ export class WelcomeComponent {
     this.appService.submitUrl(this.youtubeUrl).subscribe(
       (response) => {
         this.appService.setFormSubmitted();
-        this.router.navigate(['/download']);
+        // this.router.navigate(['/download']);
         this.loading = false;
+        let redirect: any = "http://65.2.170.186:8000/static/" + response.video_file
+        window.location.href = redirect
+        localStorage.setItem("responce", JSON.stringify(response))
       },
       (error) => {
         this.loading = false;
